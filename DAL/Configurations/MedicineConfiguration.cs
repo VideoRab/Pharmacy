f7dbf7eed47ae8dfd25e4bb7a985a936046aa78a
Roma <romabelka43@gmail.com>
2023-05-12 04:11:05 +0300

    Dal has been added

---
+++ DAL/Configurations/MedicineConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configurations;
+
+public class MedicineConfiguration : IEntityTypeConfiguration<Medicine>
+{
+    public void Configure(EntityTypeBuilder<Medicine> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Order)
+            .WithMany(x => x.Medicines)
+            .HasForeignKey(x => x.OrderId);
+    }
+}
