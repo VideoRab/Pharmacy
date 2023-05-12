4d554729837f442a16a816f3dae3ef956db09c6d
Roma <romabelka43@gmail.com>
2023-05-12 04:11:35 +0300

    Identity has been added

---
+++ Identity.Web/IdentityServerSettings/DbInitializer.cs
@@ -0,0 +1,49 @@
+﻿namespace Identity.Web.IdentityServerSettings;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+public static class DbInitializer
+{
+    public static void InitializeDatabase(this IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope();
+        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        if (!context.Clients.Any())
+        {
+            foreach (var client in Configuration.GetClients())
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            foreach (var resource in Configuration.GetIdentityResources())
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+            foreach (var resource in Configuration.GetApiScopes())
+            {
+                context.ApiScopes.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+        if (!context.ApiResources.Any())
+        {
+            foreach (var resource in Configuration.GetApiResources())
+            {
+                context.ApiResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+    }
+}
