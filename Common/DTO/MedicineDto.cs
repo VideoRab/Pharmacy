76f3701d84adbe7a93e6363d10be690a664515e4
Roma <romabelka43@gmail.com>
2023-05-17 17:28:39 +0300

    DTO's has been created

---
+++ Common/DTO/MedicineDto.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Enums;
+
+namespace Entities.DTO;
+
+public class MedicineDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public MedicineType Type { get; set; }
+    public decimal Price { get; set; }
+    public bool ByRecipe { get; set; }
+    public Guid? OrderId { get; set; }
+}
---
+++ Common/DTO/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.DTO;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
