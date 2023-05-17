7238db567b42cc82c1765a870242e6218d04d7d0
Roma <romabelka43@gmail.com>
2023-05-17 17:28:54 +0300

    Mappers has been created

---
+++ Common/Mappers/MedicineMapper.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Entities.DTO;
+using Riok.Mapperly.Abstractions;
+
+namespace Common.Mappers;
+
+[Mapper]
+public partial class MedicineMapper
+{
+    public partial Medicine Map(MedicineDto medicineDto);
+    public partial MedicineDto ReverseMap(Medicine medicine);
+    public partial IEnumerable<Medicine> MapList(IEnumerable<MedicineDto> medicineDtoList);
+    public partial IEnumerable<MedicineDto> ReverseMapList(IEnumerable<Medicine> medicineList);
+}
---
+++ Common/Mappers/UserMapper.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using Entities.DTO;
+using Riok.Mapperly.Abstractions;
+
+namespace Common.Mappers;
+
+[Mapper]
+public partial class UserMapper
+{
+    public partial User Map(UserDto userDto);
+    public partial UserDto ReverseMap(User user);
+}
