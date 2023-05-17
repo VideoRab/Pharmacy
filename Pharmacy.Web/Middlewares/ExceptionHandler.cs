df6891212ed80326ec805b9ad36ff59eeed20172
Roma <romabelka43@gmail.com>
2023-05-17 17:31:08 +0300

    Exception handler has been added

---
+++ Pharmacy.Web/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace Pharmacy.Web.Middlewares;
+
+public class ExceptionHandler
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandler(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next.Invoke(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionMessageAsync(httpContext.Response, ex);
+        }
+    }
+
+    private async Task HandleExceptionMessageAsync(HttpResponse httpResponse, Exception ex)
+    {
+        httpResponse.ContentType = "application/json";
+        httpResponse.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var message = new
+        {
+            StatusCode = httpResponse.StatusCode,
+            ErrorMessage = ex.Message,
+            InnerExceptionMessage = ex.InnerException?.Message
+        };
+
+        await httpResponse.WriteAsJsonAsync(message);
+    }
+}
