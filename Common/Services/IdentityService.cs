b9971de798ba126cedbab505fbb73c9f0720b42b
Ilya Safonov <illia220011@gmail.com>
2023-05-17 19:16:37 +0300

    Identity service (get auth user id) has been added

---
+++ Common/Services/IdentityService.cs
@@ -0,0 +1,25 @@
+﻿using Common.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Common.Services;
+
+public class IdentityService : IIdentityService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public IdentityService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid GetUserIdentity()
+    {
+        var userIdentity = _httpContextAccessor.HttpContext?.User.FindFirst("sub")?.Value;
+        if (userIdentity is null)
+        {
+            throw new NullReferenceException("User identity not found");
+        }
+
+        return Guid.Parse(userIdentity);
+    }
+}
